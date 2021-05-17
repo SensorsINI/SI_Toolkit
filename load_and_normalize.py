febce2e745f96f724193d15e6aafc3e088373a74
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2021-05-17 14:32:18 +0200

    Spliting calculation of normalization into general and application specific part

--- load_and_normalize.py
+++ load_and_normalize.py
@@ -5,7 +5,6 @@ from time import sleep
 
 import glob
 import os
-from CartPole.cartpole_model import P_GLOBALS
 
 import matplotlib.pyplot as plt
 
@@ -20,6 +19,8 @@ try:
     from git import Repo
 except:
     pass
+
+from SI_Toolkit_ApplicationSpecificFiles.user_defined_normalization_correction import apply_user_defined_normalization_correction
 
 PATH_TO_NORMALIZATION_INFO = './SI_Toolkit/NormalizationInfo/'
 PATH_TO_EXPERIMENT_RECORDINGS = './ExperimentRecordings/Train/'  # Path where the experiments data is stored
@@ -253,64 +254,7 @@ def calculate_normalization_info(paths_to_data_information=None, plot_histograms
 
     # User defined normalization values:
 
-    df_norm_info.loc['mean', 'angle'] = 0.0
-    df_norm_info.loc['std', 'angle'] = df_norm_info.loc['std', 'angle']
-    df_norm_info.loc['max', 'angle'] = np.pi
-    df_norm_info.loc['min', 'angle'] = -np.pi
-
-    df_norm_info.loc['mean', 'angleD'] = 0.0
-    df_norm_info.loc['std', 'angleD'] = df_norm_info.loc['std', 'angleD']
-    df_norm_info.loc['max', 'angleD'] = max(abs(df_norm_info.loc['max', 'angleD']),
-                                            abs(df_norm_info.loc['min', 'angleD']))
-    df_norm_info.loc['min', 'angleD'] = - df_norm_info.loc['max', 'angleD']
-
-    df_norm_info.loc['mean', 'angleDD'] = 0.0
-    df_norm_info.loc['std', 'angleDD'] = df_norm_info.loc['std', 'angleDD']
-    df_norm_info.loc['max', 'angleDD'] = max(abs(df_norm_info.loc['max', 'angleDD']),
-                                             abs(df_norm_info.loc['min', 'angleDD']))
-    df_norm_info.loc['min', 'angleDD'] = - df_norm_info.loc['max', 'angleDD']
-
-    df_norm_info.loc['mean', 'angle_cos'] = 0.0
-    df_norm_info.loc['std', 'angle_cos'] = df_norm_info.loc['std', 'angle_cos']
-    df_norm_info.loc['max', 'angle_cos'] = 1.0
-    df_norm_info.loc['min', 'angle_cos'] = - 1.0
-
-    df_norm_info.loc['mean', 'angle_sin'] = 0.0
-    df_norm_info.loc['std', 'angle_sin'] = df_norm_info.loc['std', 'angle_sin']
-    df_norm_info.loc['max', 'angle_sin'] = 1.0
-    df_norm_info.loc['min', 'angle_sin'] = - 1.0
-
-    df_norm_info.loc['mean', 'position'] = 0.0
-    df_norm_info.loc['std', 'position'] = df_norm_info.loc['std', 'position']  # no change
-    df_norm_info.loc['max', 'position'] = P_GLOBALS.TrackHalfLength
-    df_norm_info.loc['min', 'position'] = -P_GLOBALS.TrackHalfLength
-
-    df_norm_info.loc['mean', 'positionD'] = 0.0
-    df_norm_info.loc['std', 'positionD'] = df_norm_info.loc['std', 'positionD']
-    df_norm_info.loc['max', 'positionD'] = max(abs(df_norm_info.loc['max', 'positionD']),
-                                               abs(df_norm_info.loc['min', 'positionD']))
-    df_norm_info.loc['min', 'positionD'] = - df_norm_info.loc['max', 'positionD']
-
-    df_norm_info.loc['mean', 'positionDD'] = 0.0
-    df_norm_info.loc['std', 'positionDD'] = df_norm_info.loc['std', 'positionDD']
-    df_norm_info.loc['max', 'positionDD'] = max(abs(df_norm_info.loc['max', 'positionDD']),
-                                                abs(df_norm_info.loc['min', 'positionDD']))
-    df_norm_info.loc['min', 'positionDD'] = - df_norm_info.loc['max', 'positionDD']
-
-    df_norm_info.loc['mean', 'u'] = 0.0
-    df_norm_info.loc['std', 'u'] = df_norm_info.loc['std', 'u']
-    df_norm_info.loc['max', 'u'] = P_GLOBALS.u_max
-    df_norm_info.loc['min', 'u'] = - P_GLOBALS.u_max
-
-    df_norm_info.loc['mean', 'Q'] = 0.0
-    df_norm_info.loc['std', 'Q'] = df_norm_info.loc['std', 'Q']
-    df_norm_info.loc['max', 'Q'] = 1.0
-    df_norm_info.loc['min', 'Q'] = - 1.0
-
-    df_norm_info.loc['mean', 'target_position'] = 0.0
-    df_norm_info.loc['std', 'target_position'] = df_norm_info.loc['std', 'target_position']
-    df_norm_info.loc['max', 'target_position'] = P_GLOBALS.TrackHalfLength
-    df_norm_info.loc['min', 'target_position'] = -P_GLOBALS.TrackHalfLength
+    df_norm_info = apply_user_defined_normalization_correction(df_norm_info)
 
     if df_norm_info.equals(df_norm_info_from_data):
         modified = 'No'
