ed2199fc5c7d3754aaac1cde56a0f2f4799f43f2
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2021-09-02 16:37:02 +0200

    (!) Corrected ideal predictor so that no the prediction agrees with ground truth (error in next_state function)
    Change variables names over different levels of hierarchy for intermediate_steps for consistency.
    
    Removed sin and cos from output of ODE

--- Testing/Testing_Functions/get_prediction_from_euler_predictor.py
+++ Testing/Testing_Functions/get_prediction_from_euler_predictor.py
@@ -5,14 +5,14 @@ from SI_Toolkit_ApplicationSpecificFiles.predictors_customization import STATE_V
 from SI_Toolkit_ApplicationSpecificFiles.predictor_ideal import predictor_ideal
 
 DEFAULT_SAMPLING_INTERVAL = 0.02  # s, Corresponds to our lab cartpole
-def get_prediction_from_euler_predictor(a, dataset, dt_sampling, dt_sampling_by_dt_fine=1):
+def get_prediction_from_euler_predictor(a, dataset, dt_sampling, intermediate_steps=1):
 
     states_0 = dataset[STATE_VARIABLES].to_numpy()[:-a.test_max_horizon, :]
     Q = dataset['Q'].to_numpy()
     Q_array = [Q[i:-a.test_max_horizon+i] for i in range(a.test_max_horizon)]
     Q_array = np.vstack(Q_array).transpose()
 
-    predictor = predictor_ideal(horizon=a.test_max_horizon, dt=dt_sampling)
+    predictor = predictor_ideal(horizon=a.test_max_horizon, dt=dt_sampling, intermediate_steps=intermediate_steps)
 
     predictor.setup(initial_state=states_0, prediction_denorm=True)
     output_array = predictor.predict(Q_array)  # Should be shape=(a.test_max_horizon, a.test_len, len(outputs))
--- Testing/run_brunton_test.py
+++ Testing/run_brunton_test.py
@@ -18,7 +18,7 @@ def run_brunton_test():
     predictions_list = []
     for test_idx in range(len(a.tests)):
         if a.tests[test_idx] == 'Euler':
-            predictions = get_prediction_from_euler_predictor(a, dataset, dataset_sampling_dt, dt_sampling_by_dt_fine=10)
+            predictions = get_prediction_from_euler_predictor(a, dataset, dataset_sampling_dt, intermediate_steps=10)
         else:  # Assume this is a neural_network test:
             predictions = get_data_for_gui_TF(a, dataset, net_name=a.tests[test_idx])
 
