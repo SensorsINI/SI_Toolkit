d123bd975b230b722c3ffc7472439eadbe144593
asudeeaydin <OhiO123>
2022-02-15 14:31:41 +0100

    Added error message to preprocess_for_brunton.py to indicate that the file was not found.

--- Testing/Testing_Functions/preprocess_for_brunton.py
+++ Testing/Testing_Functions/preprocess_for_brunton.py
@@ -7,6 +7,12 @@ def preprocess_for_brunton(a):
     # Get dataset:
     path_to_testfile = get_full_paths_to_csvs(default_locations=a.default_locations_for_testfile, csv_names=a.test_file)
     test_dfs = load_data(path_to_testfile)
+    if not test_dfs:  # Tests for empty list
+        raise FileNotFoundError('The load_data(path_to_testfile) function returned an empty list of files. \n'
+                                'Probably the requested experiment recording is not found at the indicated location or under indicated name. \n'
+                                'path_to_testfile value is {}, \n'
+                                'a.default_locations_for_testfile is {}, \n'
+                                'a.test_file is {}'.format(path_to_testfile, a.default_locations_for_testfile, a.test_file))
     if a.test_len == 'max':
         a.test_len = len(test_dfs[0]) - a.test_max_horizon - a.test_start_idx  # You could have +1; then, for last prediction you don not have ground truth to compare with, but you can still calculate it.
     dataset = test_dfs[0].iloc[a.test_start_idx:a.test_start_idx + a.test_len + a.test_max_horizon, :]
