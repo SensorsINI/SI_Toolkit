d8828896390bdc696a121180c265f2ffd0b7d8aa
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2022-02-16 23:04:44 +0100

    Simplifying predictor_autoregressive_tf.py as much as possible.

--- TF/TF_Functions/predictor_autoregressive_tf.py
+++ TF/TF_Functions/predictor_autoregressive_tf.py
@@ -8,18 +8,16 @@ While designing the controller you just chose the predictor you want,
 """
 
 """
-This is a predictor for autoregressive RNNs constructed in tensorflowrol
-This predictor is good only for one control input being first net input, all other net inputs in the same order
-as net outputs, and all net outputs being closed loop, no dt, no target position
-horizon cannot be changed in runtime
+This is a predictor for autoregressive neural network constructed in tensorflow
+Control inputs should be first (regarding vector indices) inputs of the vector.
+all other net inputs in the same order as net outputs
 """
-
 
 """
 Using predictor:
 1. Initialize while initializing controller
-    This step load the RNN - it make take quite a bit of time
-    During initialization you only need to provide RNN which should be loaded
+    This step load the RNN - it may take quite a bit of time
+    During initialization you need to provide RNN which should be loaded
 2. Call iterativelly three functions
     a) setup(initial_state, horizon, etc.)
     b) predict(Q)
@@ -27,7 +25,7 @@ Using predictor:
     
     ad a) at this stage you can change the parameters for prediction like e.g. horizon, dt
             It also prepares 0 state of the prediction, and tensors for saving the results,
-            to make b) max performance. This function should be called BEFORE starting solving an optim
+            to make b) max performance. This function should be called BEFORE starting solving an optimisation problem
     ad b) predict is optimized to get the prediction of future states of the system as fast as possible.
         It accepts control input (vector) as its only input and is intended to be used at every evaluation of the cost functiomn
     ad c) this method updates the internal state of RNN. It accepts control input for current time step (scalar) as its only input
@@ -35,23 +33,24 @@ Using predictor:
             
 """
 
-#TODO: for the moment it is not possible to update RNN more often than mpc dt
-#   Updating it more often will lead to false results.
+# TODO: Make horizon updatable in runtime
 
 # "Command line" parameters
 from SI_Toolkit.TF.TF_Functions.Initialization import get_net, get_norm_info_for_net
 from SI_Toolkit.TF.TF_Functions.Network import get_internal_states, load_internal_states
-from SI_Toolkit.load_and_normalize import *
+from SI_Toolkit.load_and_normalize import denormalize_numpy_array, normalize_numpy_array
 
 try:
-    from SI_Toolkit_ApplicationSpecificFiles.predictors_customization import STATE_VARIABLES, STATE_INDICES, CONTROL_INPUTS, augment_predictor_output
+    from SI_Toolkit_ApplicationSpecificFiles.predictors_customization import STATE_VARIABLES, STATE_INDICES, \
+        CONTROL_INPUTS, augment_predictor_output
 except ModuleNotFoundError:
     print('SI_Toolkit_ApplicationSpecificFiles not yet created')
 
 import numpy as np
 
 from types import SimpleNamespace
-import yaml, os
+import os
+import yaml
 
 import tensorflow as tf
 
@@ -60,15 +59,15 @@ config = yaml.load(open(os.path.join('SI_Toolkit_ApplicationSpecificFiles', 'con
 NET_NAME = config['modeling']['NET_NAME']
 PATH_TO_MODELS = config["paths"]["PATH_TO_EXPERIMENT_FOLDERS"] + config['paths']['path_to_experiment'] + "Models/"
 
+
 class predictor_autoregressive_tf:
     def __init__(self, horizon=None, batch_size=None, net_name=None):
 
-        a = SimpleNamespace()
         self.batch_size = batch_size
-        self._horizon = None  # Helper variable for horizon settoer
         self.horizon = horizon
-        a.path_to_models = PATH_TO_MODELS
 
+        a = SimpleNamespace()
+        a.path_to_models = PATH_TO_MODELS
         a.net_name = net_name
 
         # Create a copy of the network suitable for inference (stateful and with sequence length one)
@@ -78,17 +77,12 @@ class predictor_autoregressive_tf:
 
         self.normalization_info = get_norm_info_for_net(self.net_info)
 
-        # Make a prediction
-
         self.rnn_internal_states = get_internal_states(self.net)
 
         self.net_initial_input_without_Q = np.zeros([len(self.net_info.inputs) - len(CONTROL_INPUTS)], dtype=np.float32)
 
-        self.prediction_denorm = None # Set to True or False in setup, determines if output should be denormalized
-
-        self.output_array = np.zeros([self.batch_size, self.horizon+1, len(STATE_VARIABLES)+len(CONTROL_INPUTS)], dtype=np.float32)
-
-        self.output_array_single_step = np.zeros([self.batch_size, 2, len(STATE_VARIABLES)+1], dtype=np.float32)
+        self.output_array = np.zeros([self.batch_size, self.horizon + 1, len(STATE_VARIABLES) + len(CONTROL_INPUTS)],
+                                     dtype=np.float32)
 
         print('Init done')
 
@@ -96,26 +90,20 @@ class predictor_autoregressive_tf:
 
         self.output_array[..., 0, :-1] = initial_state
 
-        initial_input_net_without_Q = initial_state[..., [STATE_INDICES.get(key) for key in self.net_info.inputs[len(CONTROL_INPUTS):]]]
-        self.net_initial_input_without_Q = normalize_numpy_array(initial_input_net_without_Q, self.net_info.inputs[len(CONTROL_INPUTS):], self.normalization_info)
+        initial_input_net_without_Q = initial_state[
+            ..., [STATE_INDICES.get(key) for key in self.net_info.inputs[len(CONTROL_INPUTS):]]]
+        self.net_initial_input_without_Q = normalize_numpy_array(initial_input_net_without_Q,
+                                                                 self.net_info.inputs[len(CONTROL_INPUTS):],
+                                                                 self.normalization_info)
 
-        # [1:] excludes Q which is not included in initial_state_normed
-        # As the only feature written with big Q it should be first on each list.
         self.net_initial_input_without_Q_TF = tf.convert_to_tensor(self.net_initial_input_without_Q, tf.float32)
-        self.net_initial_input_without_Q_TF = tf.reshape(self.net_initial_input_without_Q_TF, [-1, len(self.net_info.inputs[1:])])
-        if prediction_denorm:
-            self.prediction_denorm=True
-        else:
-            self.prediction_denorm = False
+        self.net_initial_input_without_Q_TF = tf.reshape(self.net_initial_input_without_Q_TF,
+                                                         [-1, len(self.net_info.inputs[1:])])
 
-        # print('Setup done')
 
-    def predict(self, Q, single_step=False) -> np.array:
+    def predict(self, Q) -> np.array:
 
-        if single_step:
-            output_array = self.output_array_single_step
-        else:
-            output_array = self.output_array
+        output_array = self.output_array
 
         output_array[..., :-1, -1] = Q
 
@@ -133,6 +121,35 @@ class predictor_autoregressive_tf:
 
         return output_array
 
+    @tf.function(experimental_compile=True)
+    def iterate_net(self, Q, net_initial_input_without_Q_TF):
+
+        net_outputs = tf.TensorArray(tf.float32, size=self.horizon)
+        net_output = tf.zeros(shape=(self.batch_size, len(self.net_info.outputs)), dtype=tf.float32)
+
+        Q = tf.transpose(Q)
+
+        for i in tf.range(self.horizon):
+            Q_current = tf.expand_dims(Q[i], axis=1)
+
+            if i == 0:
+                net_input = tf.reshape(
+                    tf.concat([Q_current, net_initial_input_without_Q_TF], axis=1),
+                    shape=[-1, 1, len(self.net_info.inputs)])
+            else:
+                net_input = tf.reshape(
+                    tf.concat([Q_current, net_output], axis=1),
+                    shape=[-1, 1, len(self.net_info.inputs)])
+
+            net_output = self.net(net_input)
+
+            net_output = tf.reshape(net_output, [-1, len(self.net_info.outputs)])
+
+            net_outputs = net_outputs.write(i, net_output)
+
+        net_outputs = tf.transpose(net_outputs.stack(), perm=[1, 0, 2])
+
+        return net_outputs
 
     # @tf.function
     def update_internal_state(self, Q0):
@@ -147,34 +164,7 @@ class predictor_autoregressive_tf:
         else:
             net_input = (tf.reshape(tf.concat([Q0, self.net_initial_input_without_Q_TF], axis=1),
                                     [-1, 1, len(self.net_info.inputs)]))
-        # self.evaluate_net(self.net_current_input) # Using tf.function to compile net
+
         self.net(net_input)  # Using net directly
 
         self.rnn_internal_states = get_internal_states(self.net)
-
-    @tf.function(experimental_compile=True)
-    def iterate_net(self, Q, net_initial_input_without_Q_TF):
-
-        net_outputs = tf.TensorArray(tf.float32, size=self.horizon)
-        net_output = tf.zeros(shape=(self.batch_size, len(self.net_info.outputs)), dtype=tf.float32)
-
-        Q = tf.transpose(Q)
-
-        for i in tf.range(self.horizon):
-            Q_current = tf.expand_dims(Q[i], axis=1)
-
-            if i == 0:
-                    net_input = (tf.reshape(tf.concat([Q_current, net_initial_input_without_Q_TF], axis=1), [-1, 1, len(self.net_info.inputs)]))
-            else:
-                    net_input = tf.reshape(tf.concat([Q_current, net_output], axis=1), [-1, 1, len(self.net_info.inputs)])
-            # net_output = self.net(net_input)
-            net_output = self.net(net_input)
-            #tf.print(net_output)
-
-            net_output = tf.reshape(net_output, [-1, len(self.net_info.outputs)])
-
-            net_outputs = net_outputs.write(i, net_output)
-            # tf.print(net_inout.read(i+1))
-        # print(net_inout)
-        net_outputs = tf.transpose(net_outputs.stack(), perm=[1, 0, 2])
-        return net_outputs
