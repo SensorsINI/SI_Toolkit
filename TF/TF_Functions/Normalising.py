63832a939332e78bdcf126350f4877795f3929a0
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2022-03-10 16:42:52 +0100

    New functions for normalizing - not deployed yet

---
+++ TF/TF_Functions/Normalising.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+
+"""
+In the below functions normalizing_array is required 
+to have columns in the same order as the features of (de)normalized_array
+and rows in the order
+0 -> mean
+1 -> std
+2 -> max
+3 -> min
+"""
+
+
+@tf.function(experimental_compile=True)
+def normalize_tf(denormalized_array,
+                 normalizing_array,
+                 normalization_type='minmax_sym'
+                 ):
+
+    if normalization_type == 'gaussian':
+        normalized_array = (denormalized_array - normalizing_array[0, :]) / normalizing_array[1, :]
+
+    elif normalization_type == 'minmax_pos':
+        normalized_array = (denormalized_array - normalizing_array[3, :]) / (
+                    normalizing_array[2, :] - normalizing_array[3, :])
+
+
+    elif normalization_type == 'minmax_sym':
+        normalized_array = -1.0 + 2.0 * (
+                (denormalized_array - normalizing_array[3, :]) / (normalizing_array[2, :] - normalizing_array[3, :])
+        )
+
+    return normalized_array
+
+
+@tf.function(experimental_compile=True)
+def denormalize_tf(normalized_array,
+                   denormalizing_array,
+                   normalization_type='minmax_sym'):
+
+    if normalization_type == 'gaussian':
+        denormalized_array = normalized_array * denormalizing_array[1, :] + denormalizing_array[0, :]
+
+    elif normalization_type == 'minmax_pos':
+        denormalized_array = normalized_array * (denormalizing_array[2, :] - denormalizing_array[3, :]) + \
+                                               denormalizing_array[3, :]
+    elif normalization_type == 'minmax_sym':
+        denormalized_array = ((normalized_array + 1.0) / 2.0) * \
+                                               (denormalizing_array[2, :] - denormalizing_array[3, :]) \
+                                               + denormalizing_array[3, :]
+
+    return denormalized_array
