9ff793c69022c2dd3083504176531aedd77b29e2
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2022-08-08 13:22:34 +0200

    New solution for non-numeric values in normalization info

--- src/SI_Toolkit/load_and_normalize.py
+++ src/SI_Toolkit/load_and_normalize.py
@@ -444,7 +444,7 @@ def normalize_feature(feature, normalization_info, normalization_type='minmax_sy
     else:
         pass
 
-    if name in normalization_info.columns:
+    if name in normalization_info.columns and pd.api.types.is_numeric_dtype(normalization_info[name]):
         pass
     else:
         return feature
@@ -466,8 +466,6 @@ def normalize_feature(feature, normalization_info, normalization_type='minmax_sy
     elif normalization_type == 'minmax_sym':
         col_min = normalization_info.loc['min', name]
         col_max = normalization_info.loc['max', name]
-        if isinstance(col_max, str):
-            return 0
         if (col_max - col_min) == 0:
             return 0
         else:
@@ -495,6 +493,11 @@ def denormalize_feature(feature, normalization_info, normalization_type='minmax_
         name = feature.name
     else:
         pass
+
+    if name in normalization_info.columns and pd.api.types.is_numeric_dtype(normalization_info[name]):
+        pass
+    else:
+        return feature
 
     if normalization_type == 'gaussian':
         # col_mean = normalization_info.loc['mean', name]
