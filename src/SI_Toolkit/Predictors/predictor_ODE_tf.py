f6b04a132977a2eb2db4cb834673baaf98da31b5
heetmeyer <heerfn@yahoo.de>
2022-10-15 19:35:29 +0200

    Add missing import

--- src/SI_Toolkit/Predictors/predictor_ODE_tf.py
+++ src/SI_Toolkit/Predictors/predictor_ODE_tf.py
@@ -2,7 +2,7 @@ from typing import Callable, Optional
 
 from Control_Toolkit.others.environment import TensorType
 
-from SI_Toolkit_ASF.predictors_customization_tf import next_state_predictor_ODE_tf
+from SI_Toolkit_ASF.predictors_customization_tf import next_state_predictor_ODE_tf, STATE_VARIABLES
 from SI_Toolkit.Functions.TF.Compile import CompileTF
 from SI_Toolkit.Predictors import template_predictor
 
