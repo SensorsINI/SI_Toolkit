e0458af621c580b8799c580ed75152932af36480
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2022-10-29 18:48:34 +0200

    updated predictor_autoregressive_GP.py and comments at predictor_autoregressive_neural.py

--- src/SI_Toolkit/Predictors/predictor_autoregressive_neural.py
+++ src/SI_Toolkit/Predictors/predictor_autoregressive_neural.py
@@ -8,29 +8,8 @@ While designing the controller you just chose the predictor you want,
 """
 
 """
-This is a predictor for autoregressive neural network constructed in tensorflow
 Control inputs should be first (regarding vector indices) inputs of the vector.
 all other net inputs in the same order as net outputs
-"""
-
-"""
-Using predictor:
-1. Initialize while initializing controller
-    This step load the RNN - it may take quite a bit of time
-    During initialization you need to provide RNN which should be loaded
-2. Call iterativelly three functions
-    a) setup(initial_state, horizon, etc.)
-    b) predict(Q)
-    c) update_net
-    
-    ad a) at this stage you can change the parameters for prediction like e.g. horizon, dt
-            It also prepares 0 state of the prediction, and tensors for saving the results,
-            to make b) max performance. This function should be called BEFORE starting solving an optimisation problem
-    ad b) predict is optimized to get the prediction of future states of the system as fast as possible.
-        It accepts control input (vector) as its only input and is intended to be used at every evaluation of the cost functiomn
-    ad c) this method updates the internal state of RNN. It accepts control input for current time step (scalar) as its only input
-            it should be called only after the optimization problem is solved with the control input used in simulation
-            
 """
 
 # TODO: Make horizon updatable in runtime
@@ -41,7 +20,7 @@ from SI_Toolkit.Predictors import template_predictor
 from SI_Toolkit.computation_library import TensorFlowLibrary
 
 import numpy as np
-# "Command line" parameters
+
 from SI_Toolkit.Functions.General.Initialization import (get_net,
                                                          get_norm_info_for_net)
 from SI_Toolkit.Functions.General.Normalising import (
@@ -109,6 +88,7 @@ class predictor_autoregressive_neural(template_predictor):
             a.net_name = model_name
 
         # Create a copy of the network suitable for inference (stateful and with sequence length one)
+
         self.net, self.net_info = \
             get_net(a, time_series_length=1,
                     batch_size=self.batch_size, stateful=True)
