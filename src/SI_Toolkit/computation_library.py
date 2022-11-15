f631135223cad455783fb16bb2b4c7a761a08972
heetmeyer <heerfn@yahoo.de>
2022-11-15 12:28:16 +0100

    Add ones_like, cumprod to Computation Libs

--- src/SI_Toolkit/computation_library.py
+++ src/SI_Toolkit/computation_library.py
@@ -57,6 +57,7 @@ class ComputationLibrary:
     zeros: Callable[["tuple[int]"], TensorType] = None
     zeros_like: Callable[[TensorType], TensorType] = None
     ones: Callable[["tuple[int]"], TensorType] = None
+    ones_like: Callable[[TensorType], TensorType] = None
     sign: Callable[[TensorType], TensorType] = None
     create_rng: Callable[[int], RandomGeneratorType] = None
     standard_normal: Callable[[RandomGeneratorType, "tuple[int]"], TensorType] = None
@@ -64,6 +65,7 @@ class ComputationLibrary:
         [RandomGeneratorType, "tuple[int]", TensorType, TensorType, type], TensorType
     ] = None
     sum: Callable[[TensorType, "Optional[Union[tuple[int], int]]"], TensorType] = None
+    cumprod: Callable[[TensorType, int], TensorType] = None
     set_shape: Callable[[TensorType, "list[int]"], None] = None
     concat: Callable[["list[TensorType]", int], TensorType]
     pi: TensorType = None
@@ -127,6 +129,7 @@ class NumpyLibrary(ComputationLibrary):
     zeros = np.zeros
     zeros_like = np.zeros_like
     ones = np.ones
+    ones_like = np.ones_like
     sign = np.sign
     create_rng = lambda seed: Generator(SFC64(seed))
     standard_normal = lambda generator, shape: generator.standard_normal(size=shape)
@@ -134,6 +137,7 @@ class NumpyLibrary(ComputationLibrary):
         low=low, high=high, size=shape
     ).astype(dtype)
     sum = lambda x, a: np.sum(x, axis=a, keepdims=False)
+    cumprod = lambda x, a: np.cumprod(x, axis=a)
     set_shape = lambda x, shape: x
     concat = lambda x, axis: np.concatenate(x, axis=axis)
     pi = np.array(np.pi).astype(np.float32)
@@ -197,6 +201,7 @@ class TensorFlowLibrary(ComputationLibrary):
     zeros = tf.zeros
     zeros_like = tf.zeros_like
     ones = tf.ones
+    ones_like = tf.ones_like
     sign = tf.sign
     create_rng = lambda seed: tf.random.Generator.from_seed(seed)
     standard_normal = lambda generator, shape: generator.normal(shape)
@@ -204,6 +209,7 @@ class TensorFlowLibrary(ComputationLibrary):
         shape, minval=low, maxval=high, dtype=dtype
     )
     sum = lambda x, a: tf.reduce_sum(x, axis=a, keepdims=False)
+    cumprod = lambda x, a: tf.math.cumprod(x, axis=a)
     set_shape = lambda x, shape: x.set_shape(shape)
     concat = lambda x, axis: tf.concat(x, axis)
     pi = tf.convert_to_tensor(np.array(np.pi), dtype=tf.float32)
@@ -272,6 +278,7 @@ class PyTorchLibrary(ComputationLibrary):
     zeros = torch.zeros
     zeros_like = torch.zeros_like
     ones = torch.ones
+    ones_like = torch.ones_like
     sign = torch.sign
     create_rng = lambda seed: torch.Generator().manual_seed(seed)
     standard_normal = lambda generator, shape: torch.normal(
@@ -283,6 +290,7 @@ class PyTorchLibrary(ComputationLibrary):
         + low
     )
     sum = lambda x, a: torch.sum(x, a, keepdim=False)
+    cumprod = lambda x, a: torch.cumprod(x, dim=a)
     set_shape = lambda x, shape: x
     concat = lambda x, axis: torch.concat(x, dim=axis)
     pi = torch.from_numpy(np.array(np.pi)).float()
