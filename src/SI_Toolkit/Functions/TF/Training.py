5c8f53bb43d3fce9cead14cc232857263402fb6e
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2023-01-08 21:39:58 +0100

    Revert a change unnecessarily fixing the learning rate in TF neural net training. CAUTION!!! This causes the default lr to change from e-3 to e-2

--- src/SI_Toolkit/Functions/TF/Training.py
+++ src/SI_Toolkit/Functions/TF/Training.py
@@ -47,7 +47,7 @@ def train_network_core(net, net_info, training_dfs_norm, validation_dfs_norm, te
         loss=loss_msr_sequence_customizable(wash_out_len=a.wash_out_len,
                                             post_wash_out_len=a.post_wash_out_len,
                                             discount_factor=1.0),
-        optimizer=keras.optimizers.Adam(0.001)
+        optimizer=keras.optimizers.Adam(a.lr)
     )
 
     # region Define callbacks to be used in training
