29bd6558800b8fa7c87c819c61039f6f776e62f8
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2024-02-12 05:17:29 +0100

    Delayed commit of value_precision.py update.

--- src/SI_Toolkit/Functions/General/value_precision.py
+++ src/SI_Toolkit/Functions/General/value_precision.py
@@ -1,9 +1,8 @@
 # This is equivalent to the functions we used in C, up to the wrapping of negative numbers
+from SI_Toolkit.computation_library import NumpyLibrary
 
-import numpy as np
 
-
-def float_to_ap_fixed(value, total_bits=20, integer_bits=6, rounding=True):
+def float_to_ap_fixed(value, total_bits=20, integer_bits=6, rounding=True, lib=NumpyLibrary):
     # Calculate the scale for the fractional part
     scale = 2 ** (total_bits - integer_bits)
 
@@ -12,9 +11,9 @@ def float_to_ap_fixed(value, total_bits=20, integer_bits=6, rounding=True):
 
     # Round or truncate using NumPy functions for vectorized operations
     if rounding:
-        fixed_point_value = np.round(scaled_value).astype(int)
+        fixed_point_value = lib.cast(lib.rint(scaled_value), lib.int32)
     else:
-        fixed_point_value = np.floor(scaled_value).astype(int)  # Using floor for truncation
+        fixed_point_value = lib.cast(lib.floor(scaled_value), lib.int32)  # Using floor for truncation
 
     # Maximum and minimum representable values calculation
     max_val = 2 ** (total_bits - 1) - 1
@@ -25,11 +24,11 @@ def float_to_ap_fixed(value, total_bits=20, integer_bits=6, rounding=True):
     fixed_point_value = (fixed_point_value - min_val) % (max_val - min_val + 1) + min_val
 
     # Convert back to float to simulate fixed-point precision
-    return fixed_point_value / scale
+    return lib.cast(fixed_point_value / scale, lib.float32)
 
 
 
-def set_value_precision(value, precision):
+def set_value_precision(value, precision, lib=NumpyLibrary):
     if precision == 'float':
         pass
     elif precision[:len('ap_fixed')] == 'ap_fixed':
@@ -37,5 +36,5 @@ def set_value_precision(value, precision):
         precision = precision.split(',')
         total_bits = int(precision[0])
         integer_bits = int(precision[1])
-        value = float_to_ap_fixed(value, total_bits=total_bits, integer_bits=integer_bits, rounding=True)
+        value = float_to_ap_fixed(value, total_bits=total_bits, integer_bits=integer_bits, rounding=True, lib=lib)
     return value
