88bc1c848bfa2664c9f8a370f9d092db8ddd1108
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2024-04-10 17:06:28 +0200

    Updates of naming

--- src/SI_Toolkit/Predictors/predictor_autoregressive_neural.py
+++ src/SI_Toolkit/Predictors/predictor_autoregressive_neural.py
@@ -185,7 +185,7 @@ class predictor_autoregressive_neural(template_predictor):
         if self.net_info.normalize:
             self.denormalize_outputs = get_denormalization_function(self.normalization_info, outputs_names, self.lib)
 
-        self.augmentation = predictor_output_augmentation_tf(self.net_info, self.lib, differential_network=self.differential_network)
+        self.augmentation = predictor_output_augmentation(self.net_info, self.lib, differential_network=self.differential_network)
 
         indices_outputs_rev = [self.predictor_output_indices.get(key, np.inf) for key in outputs_names+self.augmentation.features_augmentation]
         missing_indices_output = [i for i in range(len(self.predictor_output_indices)) if i not in indices_outputs_rev]
