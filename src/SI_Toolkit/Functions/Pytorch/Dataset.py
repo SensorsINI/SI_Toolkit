95bc9040b81202dce84bbca91b2413b54be57819
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2025-05-06 03:47:35 +0200

    Removing some snippet remaining from translation invariant variables

--- src/SI_Toolkit/Functions/Pytorch/Dataset.py
+++ src/SI_Toolkit/Functions/Pytorch/Dataset.py
@@ -15,12 +15,6 @@ class Dataset(DatasetTemplate, data.Dataset):
         super(Dataset, self).__init__(dfs, args, inputs, outputs, exp_len, shuffle,
                                       normalization_info=normalization_info)
 
-        if self.tiv:
-            # FIXME: tiv not working here for Pytorch as it requires change at each epoch end
-            raise Exception('TIV in Pytorch might now work as expected.'
-                            'If you know what you do comment this line out (but do not commit!)'
-                            'Otherwise provide as TIV empty list.')
-
     def __len__(self):
         return self.number_of_samples_to_use
 
